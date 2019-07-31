8e06fbb1b25bbc6b6955f663427c45a2035cd272
Aditya Wiraha <squalvj@gmail.com>
2019-07-31 12:50:40 +0800

    add redux thunk

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './container/Home'
 import Header from 'components/Header'
+import { Provider } from 'react-redux'
 import './styles/css/core.scss';
 export default class App extends Component {
   render() {
+    const {
+      store
+    } = this.props
     return (
-      <Router>
-        <Header />
-        <div id="app">
-          <Route exact path="/" component={Home} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <div id="app">
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
+      </Provider>
       )
     }
 }
